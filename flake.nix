0f37420d2d4b95545ac1e53a0cfe6bec6587d08d
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2023-01-05 14:05:45 -0300

    Init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix wrapped development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/22.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
+          rec {
+            buildInputs = with pkgs;
+              [
+                go_1_18
+                gopls
+                goreleaser
+                gnumake
+              ];
+          };
+      });
+}
