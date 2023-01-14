fa564cb488fef18ef8b2551ec43449d2a18e660f
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2023-01-14 17:35:44 -0300

    Initial 12factor config remap

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/pecigonzalo/kafka-canary/pkg/api"
@@ -29,15 +30,32 @@ var (
 	}, nil)
 )
 
+type Config struct {
+	Host    string        `mapstructure:"host"`
+	Port    int           `mapstructure:"port"`
+	Level   string        `mapstructure:"level"`
+	Brokers []string      `mapstructure:"brokers"`
+	Canary  canary.Config `mapstructure:"canary"`
+	Output  string        `mapstructure:"output"`
+}
+
 func main() {
 	fs := pflag.NewFlagSet("default", pflag.ContinueOnError)
 	fs.String("host", "", "Host to bind service to")
 	fs.Int("port", 9898, "HTTP port to bind service to")
-	fs.String("level", "info", "log level debug, info, warn, error, fatal or panic")
+	fs.StringSlice("brokers", []string{}, "Kafka broker address")
+	fs.String("output", "json", "Output target [console, json]")
+	fs.String("level", "info", "Log level [debug, info, warn, error, fatal, panic]")
+	fs.String("canary.topic", "__kafka_canary", "Name of the topic used by the canary")
+	fs.String("canary.client-id", "kafka-canary", "Id of the producer used by the canary")
+	fs.String("canary.consumer-group-id", "kafka-canary-group", "Id of the consumer group used by the canary")
 	versionFlag := fs.BoolP("version", "v", false, "get version number")
 
-	// bind flags and environment variables
+	// Bind flags and environment variables
+	viper.SetEnvPrefix("KAFKA_CANARY")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.BindPFlags(fs)
+	viper.AutomaticEnv()
 
 	// parse flags
 	err := fs.Parse(os.Args[1:])
@@ -53,70 +71,70 @@ func main() {
 		os.Exit(0)
 	}
 
-	// setup logger
-	level, err := zerolog.ParseLevel(viper.GetString("level"))
+	// Load config
+	var config Config
+	if err = viper.Unmarshal(&config); err != nil {
+		fmt.Fprintf(os.Stderr, "Config unmarshal failed: %s\n\n", err.Error())
+		os.Exit(2)
+	}
+	// TODO: Make me flags
+	// Configure the canary services
+	// canaryConfig.ProducerLatencyBuckets =       []float64{2, 5, 10, 20, 50, 100, 200, 400}
+	config.Canary.ProducerLatencyBuckets = []float64{100, 500, 1000, 1500, 2000, 4000, 8000}
+	// canaryConfig.EndToEndLatencyBuckets =       []float64{5, 10, 20, 50, 100, 200, 400, 800}
+	config.Canary.EndToEndLatencyBuckets = []float64{100, 500, 1000, 2000, 8000, 10000, 12000, 15000}
+	config.Canary.ReconcileInterval = 5 * time.Second
+	config.Canary.StatusCheckInterval = 30 * time.Second
+	config.Canary.BootstrapBackoffMaxAttempts = 10
+	config.Canary.BootstrapBackoffScale = 5 * time.Second
+
+	// Setup logger
+	level, err := zerolog.ParseLevel(config.Level)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n\n", err.Error())
 		os.Exit(2)
 	}
 	zerolog.SetGlobalLevel(level)
-	// TODO: Change to conditional
-	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().
+
+	var logger zerolog.Logger
+	if config.Output == "console" {
+		logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr})
+	} else {
+		logger = zerolog.New(os.Stderr)
+	}
+	logger = logger.With().
 		Timestamp().
+		Str("version", version).
 		Str("service", "kafka-canary").
 		Logger()
 
-	// validate port
-	if _, err := strconv.Atoi(viper.GetString("port")); err != nil {
-		port, _ := fs.GetInt("port")
-		viper.Set("port", strconv.Itoa(port))
-	}
-
-	// load HTTP server config
-	var srvCfg api.Config
-	if err := viper.Unmarshal(&srvCfg); err != nil {
-		logger.Panic().Err(err).Msg("Config unmarshal failed")
-	}
-
-	// start HTTP server
+	// Start HTTP server
 	logger.Info().
-		Str("version", version).
+		Str("config", fmt.Sprintf("%+v", config)).
+		Str("config", fmt.Sprintf("%+v", config)).
 		Msg("Starting Kafka Canary")
+	srvCfg := api.Config{
+		Host:    config.Host,
+		Port:    strconv.Itoa(config.Port),
+		Service: "kafka-canary",
+	}
 	srv, _ := api.NewServer(&srvCfg, &logger)
 	httpServer, healthy, ready := srv.ListenAndServe()
 
-	// setup the canary services
-	canaryConfig := canary.Config{
-		Topic:    "__kafka_canary.3",
-		ClientID: "kafka-canary-client",
-		// ProducerLatencyBuckets:      []float64{2, 5, 10, 20, 50, 100, 200, 400},
-		ProducerLatencyBuckets: []float64{100, 500, 1000, 1500, 2000, 4000, 8000},
-		// EndToEndLatencyBuckets:      []float64{5, 10, 20, 50, 100, 200, 400, 800},
-		EndToEndLatencyBuckets:      []float64{100, 500, 1000, 2000, 8000, 10000, 12000, 15000},
-		ConsumerGroupID:             "kafka-canary-group",
-		ReconcileInterval:           5 * time.Second,
-		StatusCheckInterval:         30 * time.Second,
-		BootstrapBackoffMaxAttempts: 10,
-		BootstrapBackoffScale:       5 * time.Second,
-	}
 	connectorConfig := client.ConnectorConfig{
-		BrokerAddrs: []string{
-			"b-1.streamingplatformpr.ieik6g.c8.kafka.eu-central-1.amazonaws.com:9098",
-			"b-2.streamingplatformpr.ieik6g.c8.kafka.eu-central-1.amazonaws.com:9098",
-			"b-4.streamingplatformpr.ieik6g.c8.kafka.eu-central-1.amazonaws.com:9098",
-		},
-		TLS:  client.TLSConfig{Enabled: true},
-		SASL: client.SASLConfig{Enabled: true, Mechanism: client.SASLMechanismAWSMSKIAM},
+		BrokerAddrs: config.Brokers,
+		TLS:         client.TLSConfig{Enabled: true},
+		SASL:        client.SASLConfig{Enabled: true, Mechanism: client.SASLMechanismAWSMSKIAM},
 	}
 
-	topicService := services.NewTopicService(canaryConfig, connectorConfig, &logger)
-	producerService := services.NewProducerService(canaryConfig, connectorConfig, &logger)
-	consumerService := services.NewConsumerService(canaryConfig, connectorConfig, &logger)
-	connectionService := services.NewConnectionService(canaryConfig, connectorConfig)
-	statusService := services.NewStatusServiceService(canaryConfig, &logger)
+	topicService := services.NewTopicService(config.Canary, connectorConfig, &logger)
+	producerService := services.NewProducerService(config.Canary, connectorConfig, &logger)
+	consumerService := services.NewConsumerService(config.Canary, connectorConfig, &logger)
+	connectionService := services.NewConnectionService(config.Canary, connectorConfig)
+	statusService := services.NewStatusServiceService(config.Canary, &logger)
 
 	// start canary manager
-	canaryManager := workers.NewCanaryManager(canaryConfig, topicService, producerService, consumerService, connectionService, statusService, &logger)
+	canaryManager := workers.NewCanaryManager(config.Canary, topicService, producerService, consumerService, connectionService, statusService, &logger)
 	canaryManager.Start()
 
 	// graceful shutdown
--- pkg/canary/config.go
+++ pkg/canary/config.go
@@ -3,13 +3,13 @@ package canary
 import "time"
 
 type Config struct {
-	Topic                       string
-	ClientID                    string
-	ReconcileInterval           time.Duration
-	StatusCheckInterval         time.Duration
-	BootstrapBackoffMaxAttempts int
-	BootstrapBackoffScale       time.Duration
-	ProducerLatencyBuckets      []float64
-	EndToEndLatencyBuckets      []float64
-	ConsumerGroupID             string
+	Topic                       string        `mapstructure:"topic"`
+	ClientID                    string        `mapstructure:"client-id"`
+	ReconcileInterval           time.Duration `mapstructure:"reconcile-interval"`
+	StatusCheckInterval         time.Duration `mapstructure:"status-check-interval"`
+	BootstrapBackoffMaxAttempts int           `mapstructure:"bootstrap-backoff-max-attempts"`
+	BootstrapBackoffScale       time.Duration `mapstructure:"bootstrap-backoff-scale"`
+	ProducerLatencyBuckets      []float64     `mapstructure:"producer-latency-buckets"`
+	EndToEndLatencyBuckets      []float64     `mapstructure:"endtoend-latency-buckets"`
+	ConsumerGroupID             string        `mapstructure:"consumer-group-id"`
 }
