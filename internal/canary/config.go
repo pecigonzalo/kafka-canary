5cdb276a2d4275312a2a9895550d0edd65ffed12
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2023-04-05 08:31:30 +0200

    Remove status check interval as its not in use

--- internal/canary/config.go
+++ internal/canary/config.go
@@ -6,7 +6,6 @@ type Config struct {
 	Topic                       string        `mapstructure:"topic"`
 	ClientID                    string        `mapstructure:"client-id"`
 	ReconcileInterval           time.Duration `mapstructure:"reconcile-interval"`
-	StatusCheckInterval         time.Duration `mapstructure:"status-check-interval"`
 	BootstrapBackoffMaxAttempts int           `mapstructure:"bootstrap-backoff-max-attempts"`
 	BootstrapBackoffScale       time.Duration `mapstructure:"bootstrap-backoff-scale"`
 	ProducerLatencyBuckets      []float64     `mapstructure:"producer-latency-buckets"`
